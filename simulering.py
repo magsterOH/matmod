e61b3884bfe4952191abff113f0d15c4268365fd
Anton Ståhlroos <antonstahlroos@gmail.com>
2024-04-29 17:54:34 +0200

    testa olika antal träd

--- simulering.py
+++ simulering.py
@@ -1,11 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+TREE_RADIUS_MEAN = 1
+TREE_RADIUS_VARIATION = 0.5
+
 
 class Tree:
     def __init__(self) -> None:
         self.position = self.random_position(100)
-        self.radius = self.random_radius(mean=1, variation=0.5)
+        self.radius = self.random_radius(mean=TREE_RADIUS_MEAN, variation=TREE_RADIUS_VARIATION)
 
     def random_position(self, size: int) -> np.ndarray:
         return np.random.uniform(-size / 2, size / 2, 2)
@@ -23,6 +26,9 @@ class Tree:
     @property
     def area(self) -> float:
         return np.pi * self.radius**2
+
+    def __str__(self) -> str:
+        return f"Träd | position: ({np.round(self.position[0], 2)}, {np.round(self.position[1], 2)}), radie: {np.round(self.radius, 2)}"
 
 
 class Forest:
@@ -42,26 +48,40 @@ class Forest:
         return np.array([tree.radius for tree in self.trees]) * 10
 
     def counted(self, angle) -> np.ndarray:
+        """angle: vinkel i grader"""
         return np.array([tree.inside_angle(angle) for tree in self.trees])
 
     @property
     def total_area(self) -> float:
         return sum([tree.area for tree in self.trees])
 
+    def __str__(self) -> str:
+        return f"Skog | träd: {len(self.trees)}, total area: {np.round(self.total_area, 2)}"
 
-def plot_forest(forest: Forest):
+
+def plot_forest(forest: Forest, angle: float):
     plt.scatter(forest.x_positions, forest.y_positions, s=forest.radii, c=forest.counted(angle))
+    plt.title(f"vinkel={angle}")
     plt.show()
 
 
-angle = 5
-forest = Forest(number_of_trees=500)
+if __name__ == "__main__":
+    angle = 5
+    forest = Forest(number_of_trees=500)
 
-for angle in range(1, 10):
-    print(f"Vinkel: {angle}, ", end="")
-    print(f"Räknade träd: {sum(forest.counted(angle))}, ", end="")
-    print(f"Total area: {forest.total_area}, ", end="")
-    print(f"area/träd: {forest.total_area/sum(forest.counted(angle))}")
+    # testa olika vinklar
+    for angle in range(1, 10):
+        print(f"Vinkel: {angle}, ", end="")
+        print(f"Räknade träd: {sum(forest.counted(angle))}, ", end="")
+        print(f"Total area: {forest.total_area}, ", end="")
+        print(f"area/träd: {forest.total_area/sum(forest.counted(angle))}")
 
+    angle = 5
 
-plot_forest(forest)
+    # testa olika antal träd
+    for trees in range(100, 2000, 100):
+        forest = Forest(number_of_trees=trees)
+        print(f"antal träd: {len(forest.trees)}, ", end="")
+        print(f"area/träd: {forest.total_area/sum(forest.counted(angle))}")
+
+    # plot_forest(forest, angle)
